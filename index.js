0fdeffc1088021e39b8dbc643604001bca6145fb
Marc Jones <marcjones88@mac.com>
2021-05-04 15:54:25 +0100

    Beta version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = () => {
-console.log('Hello Mars!');
+module.exports = (lang) => {
+if (lang === 'es'){
+    console.log('Hola Marte!');
+} else {
+    console.log('Hello Mars'); 
+   }
 };
